c7136b50c0c4cb1609c5a5e0b595c5b85d23b2f4
Daniel Grondin <tm.kkitsune@gmail.com>
2016-02-24 09:52:17 -0500

    Created a simple SFML RenderWindow

--- source/game/main.cpp
+++ source/game/main.cpp
@@ -1,9 +1,28 @@
+#include <SFML/Graphics.hpp>
+
 #include <iostream>
 
 using namespace std;
+using namespace sf;
 
 int main(int /*argc*/, char* /*argv*/[])
 {
-	cout << "Hello World!" << endl;
+	RenderWindow app(VideoMode(960, 640), "ProjectRune - Game", Style::Titlebar | Style::Close);
+	app.setVerticalSyncEnabled(true);
+
+	while(app.isOpen())
+	{
+		Event e;
+		while(app.pollEvent(e))
+		{
+			if(e.type == Event::KeyPressed && e.key.code == Keyboard::Escape)
+				app.close();
+		}
+
+		app.clear(Color::Black);
+
+		app.display();
+	}
+
 	return 0;
 }
